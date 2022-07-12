f13380caa75eda455d6ab4246bc1aefa49b954bd
odziminski <o.dziminski@gmail.com>
2022-07-12 13:53:01 +0200

    feat: upload screenshots to AWS S3 using SDK

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const puppeteer = require('puppeteer');
 const _ = require("underscore");
 const pg = require('pg')
 const fs = require('fs');
+const AWS = require('aws-sdk');
+const ID = 'AKIAQ22Y4SZAKAJ6QLWX';
+const SECRET = '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';
+const s3 = new AWS.S3({
+    accessKeyId: ID,
+    secretAccessKey: SECRET
+});
+
+const BUCKET_NAME = 'crawler-scraper-bucket';
 
 const pool = new pg.Pool({
-    host: "ec2-52-48-159-67.eu-west-1.compute.amazonaws.com",
-    user: "orjiloeenxaeks",
+    host: "ec2-99-81-16-126.eu-west-1.compute.amazonaws.com",
+    user: "uexrudscyqkrlu",
     port: 5432,
-    password: "6803c26e8b82bfd2e14aa5a635e3f247b4831eacd9e4fa371db1f54ab9a59a1f",
-    database: "d4cdoa4c08nksv"
+    password: "d56157f635474960d619b07b046618f031b2fd69432659dff454a8276d58947f",
+    database: "dbls649lleuqal",
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 const getImageName = async (href) => {
@@ -35,11 +47,11 @@ const crawl = async () => {
         const link = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/?search%5Bfilter_float_price%3Ato%5D=3000&search%5Bfilter_float_price%3Afrom%5D=1000&search%5Bfilter_enum_rooms%5D%5B0%5D=two';
 
         const browser = await puppeteer.launch({
-            'args' : [
-              '--no-sandbox',
-              '--disable-setuid-sandbox'
+            'args': [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
             ]
-          });
+        });
         const [page] = await browser.pages();
 
         await page.goto(link);
@@ -61,14 +73,14 @@ const crawl = async () => {
         );
 
         let advertInfo = {};
-                                
+
         const olxPriceSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > div.css-dcwlyx > h3';
         const olxAreaSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > ul > li:nth-child(5) > p';
         const olxAdditionalPaymentsSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > ul > li:nth-child(7) > p';
         const olxDistrictSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1pyxm30 > div:nth-child(2) > div > section > div.css-1nrl4q4 > div > p.css-7xdcwc-Text.eu5v0x0 > span';
 
         const otodomSelector = '#__next > main > div.css-17vqyja.e1t9fvcw3 > div.css-1sxg93g.e1t9fvcw1 > header > strong';
-            console.log(correctAdverts);
+        console.log(correctAdverts);
         let i = 0;
         for (let href of correctAdverts) {
             console.log('entering ' + href);
@@ -77,60 +89,71 @@ const crawl = async () => {
                 timeout: 0
             });
             let hrefFilename = await checkForPromotedAdvert(href);
-                let imgName = await getImageName(hrefFilename);
+            let imgName = await getImageName(hrefFilename);
             if (!fs.existsSync('img/' + imgName + '.png')) {
-                
-                await page.screenshot({ path: 'img/' + imgName + '.png', fullPage: true });
+
+                const screenshot = await page.screenshot({
+                    fullPage: true
+                });
+                const params = { Bucket: BUCKET_NAME, Key: imgName, Body: screenshot };
+                await s3.putObject(params).promise();
+                s3.upload(params, function(err, data) {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`File uploaded successfully. ${data.Location}`);
+                });
+
                 if (href.startsWith('https://www.olx.pl/d/oferta/')) {
                     console.log('success! ' + href)
-                    
-                        let price = await getData(page, olxPriceSelector, null);
-                        let additionalPayments = await getData(page, olxAdditionalPaymentsSelector, 'Czynsz');
-                        let area = await getData(page, olxAreaSelector, 'Powierzchnia: ');
-                        console.log(price, additionalPayments, area);
 
-                        let district = await getData(page, olxDistrictSelector, null, true);
-                        if (!district) {
-                            console.log('district not found at ' + href);
-                            if (await page.$(olxDistrictSelector) !== null){
-                                district = await page.evaluate(el => el.textContent, await page.$(olxDistrictSelector));   
-                            } else {
-                                district = 'undefined';
-                            }
+                    let price = await getData(page, olxPriceSelector, null);
+                    let additionalPayments = await getData(page, olxAdditionalPaymentsSelector, 'Czynsz');
+                    let area = await getData(page, olxAreaSelector, 'Powierzchnia: ');
+                    console.log(price, additionalPayments, area);
 
+                    let district = await getData(page, olxDistrictSelector, null, true);
+                    if (!district) {
+                        console.log('district not found at ' + href);
+                        if (await page.$(olxDistrictSelector) !== null) {
+                            district = await page.evaluate(el => el.textContent, await page.$(olxDistrictSelector));
+                        } else {
+                            district = 'undefined';
                         }
-                        advertInfo[i] = {
-                            'href': href,
-                            'price': price,
-                            'additionalPayments': additionalPayments,
-                            'area': area,
-                            'district': district,
-                        };
 
-                        if (district) {
-                            console.log('attempting to add a new record ' + href);
+                    }
+                    advertInfo[i] = {
+                        'href': href,
+                        'price': price,
+                        'additionalPayments': additionalPayments,
+                        'area': area,
+                        'district': district,
+                    };
 
-                            pool.connect(function (err, client, done) {
-                                if (err) {
-                                    return console.error('connexion error', err);
-                                }
-                                client.query("INSERT INTO data (price,href,additional_payments,area,district,image_name) VALUES ($1,$2,$3,$4,$5,$6) ON CONFLICT (href) DO NOTHING;", [price, href, additionalPayments, area, district,imgName], function () {
-                                    done();
+                    if (district) {
+                        console.log('attempting to add a new record ' + href);
 
+                        pool.connect(function (err, client, done) {
+                            if (err) {
+                                return console.error('connexion error', err);
+                            }
+                            client.query("INSERT INTO data (price,href,additional_payments,area,district,image_name) VALUES ($1,$2,$3,$4,$5,$6) ON CONFLICT (href) DO NOTHING;", [price, href, additionalPayments, area, district, imgName], function () {
+                                done();
 
-                                });
-                                console.log('added ' + href);
 
                             });
+                            console.log('added ' + href);
 
-                        }
-                        i++;
-                    
+                        });
+
+                    }
+                    i++;
+
 
                 } else {
                     console.log('already in DB');
                 }
-            }            
+            }
 
         }
         console.log(advertInfo);
@@ -146,10 +169,10 @@ const crawl = async () => {
 
 
 (async function main() {
-    const cron = require('node-cron');
-    cron.schedule('*/20 * * * *', function() {
+    // const cron = require('node-cron');
+    // cron.schedule('*/20 * * * *', function() {
     crawl();
-      });
+    //   });
 })();
 
 
@@ -165,3 +188,21 @@ const getData = async (page, olxSelector, word = null, district = null) => {
         } else return data.replace(/\D/g, '');
     }
 }
+
+const uploadFile = (fileName) => {
+    const fileContent = fs.readFileSync(fileName);
+
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: 'cat.jpg', // File name you want to save as in S3
+        Body: fileContent
+    };
+
+    // Uploading files to the bucket
+    s3.upload(params, function (err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+};
