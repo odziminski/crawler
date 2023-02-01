6a674f063aca93e9852ee810e869d1ceae4ec640
odziminski <o.dziminski@gmail.com>
2023-02-01 15:42:37 +0100

    code cleanup

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 if (process.env.NODE_ENV !== 'production')
     require('dotenv').config()
 
+
 const puppeteer = require('puppeteer');
 const _ = require("underscore");
-
+var cron = require('node-cron');
 
 let dbModule = require('./modules/database.js');
 let pool = dbModule.pool;
@@ -17,40 +18,56 @@ const checkForPromotedAdvert = async (href) => {
     }
     return href;
 }
-
-
+const getData = async (page, olxSelector, word = null, district = null) => {
+    if (await page.$(olxSelector) !== null) {
+        let data = await page.evaluate(el => el.textContent, await page.$(olxSelector))
+        if (district) return data;
+        if (word) {
+            if (!data.indexOf(word)) {
+                return data.replace(/[^\d.,]/g, '');
+            }
+        } else return data.replace(/\D/g, '');
+    }
+}
 
 const crawl = async () => {
     let mailModule = require('./modules/mail.js');
     let transporter = mailModule.transporter;
-    transporter.verify().then(console.log).catch(console.error);
 
     try {
 
-        const link = 'https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/?search%5Bfilter_float_price%3Ato%5D=3000&search%5Bfilter_float_price%3Afrom%5D=1000&search%5Bfilter_enum_rooms%5D%5B0%5D=two';
+        const link = process.env.OLX_CRAWL_HREF;
 
         const browser = await puppeteer.launch({
+            headless: true,
             'args': [
                 '--no-sandbox',
                 '--disable-setuid-sandbox'
             ]
         });
         const [page] = await browser.pages();
-
+        const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+        await page.setExtraHTTPHeaders({"Accept-Language": "en-US,en;q=0.9"});
+        await page.setUserAgent(ua);
         await page.goto(link);
 
         let correctAdverts = await getCorrectAdverts(browser);
         console.log(correctAdverts);
         let advertInfo = {};
 
-        const olxPriceSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > div.css-dcwlyx > h3';
-        const olxAreaSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > ul > li:nth-child(5) > p';
-        const olxAdditionalPaymentsSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1vnw4ly > div.css-1wws9er > ul > li:nth-child(7) > p';
-        const olxDistrictSelector = '#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-1pyxm30 > div:nth-child(2) > div > section > div.css-1nrl4q4 > div > p.css-7xdcwc-Text.eu5v0x0 > span';
+        const olxPriceSelector = process.env.OLX_PRICE_SELECTOR;
+        const olxAreaSelector = process.env.OLX_AREA_SELECTOR;
+        const olxAdditionalPaymentsSelector = process.env.OLX_ADDITIONAL_PAYMENTS_SELECTOR;
+        const olxDistrictSelector = process.env.OLX_DISTRICT_SELECTOR;
 
         let i = 0;
         for (let href of correctAdverts) {
+
+            // console.log(document.querySelector("#root > div.css-50cyfj > div.css-1on7yx1 > div:nth-child(3) > div.css-n9feq4 > div.css-1wws9er > div.css-dcwlyx > h3"));
             console.log(getCurrentDateString() + 'entering ' + href);
+            const ua = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+            await page.setExtraHTTPHeaders({"Accept-Language": "en-US,en;q=0.9"});
+            await page.setUserAgent(ua);
             await page.goto(href, {
                 waitUntil: 'networkidle2',
                 timeout: 0
@@ -65,7 +82,7 @@ const crawl = async () => {
                 let area = await getData(page, olxAreaSelector, 'Powierzchnia: ');
 
                 let district = await getData(page, olxDistrictSelector, null, true);
-
+                console.log(price,additionalPayments,area,district);
                 if (!district) {
                     console.log(getCurrentDateString() + ' district not found at ' + href);
                     if (await page.$(olxDistrictSelector) !== null) {
@@ -82,6 +99,9 @@ const crawl = async () => {
                     console.log(getCurrentDateString() + ' attempting to add a new record ' + href);
 
                     pool.query(insertStatement, [price, hrefChecked, additionalPayments, area, district], function (error, result) {
+                        if (error){
+                            console.log(error);
+                        }
                         if (result && result.rows[0]) {
                             console.log(getCurrentDateString() + ' added ' + href);
                             advertInfo[i] = {
@@ -91,9 +111,9 @@ const crawl = async () => {
                                 'area': area,
                                 'district': district,
                             };
-                            if (totalCost <= 2900) {
+                            if (totalCost <= 3200) {
                                 console.log('sending mail');
-                                sendMail(transporter, price, additionalPayments, href);
+                                sendMail(transporter, price, additionalPayments, href, false);
                             }
                         }
                     });
@@ -111,30 +131,9 @@ const crawl = async () => {
     }
 
 
-
 }
 
 
-(async function main() {
-    const cron = require('node-cron');
-    cron.schedule('*/20 * * * *', function () {
-        crawl();
-    });
-})();
-
-
-
-const getData = async (page, olxSelector, word = null, district = null) => {
-    if (await page.$(olxSelector) !== null) {
-        let data = await page.evaluate(el => el.textContent, await page.$(olxSelector))
-        if (district) return data;
-        if (word) {
-            if (!data.indexOf(word)) {
-                return data.replace(/[^\d.,]/g, '');
-            }
-        } else return data.replace(/\D/g, '');
-    }
-}
 
 const getCorrectAdverts = async (browser) => {
     const [page] = await browser.pages();
@@ -146,11 +145,11 @@ const getCorrectAdverts = async (browser) => {
         )
     );
 
-    const uniqueHrefs = allHrefs.filter((x, i, a) => a.indexOf(x) == i)
+    const uniqueHrefs = allHrefs.filter((x, i, a) => a.indexOf(x) === i)
     return _.filter(
         uniqueHrefs,
         function (s) {
-            return s.indexOf('https://www.olx.pl/d/oferta/') !== -1 || s.indexOf('https://www.otodom.pl/pl/oferta/') !== -1;
+            return s.indexOf('https://www.olx.pl/d/oferta/') !== -1 || s.indexOf('/d/oferta/') !== -1;
         }
     );
 
@@ -162,16 +161,30 @@ function getCurrentDateString() {
     return "[" + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + "]";
 };
 
-function sendMail(transporter, price, additionalPayments, href) {
+function sendMail(transporter, price, additionalPayments, href, onePerson) {
+    let receiver = onePerson ? "jnnkczm@gmail.com" : "magicznasowa16@gmail.com,aleksandra.kucharczyk13@gmail.com";
+    let subject = onePerson ? "🤖 Znalazłem pokój w dobrej cenie 🤖" : "🤖 Znalazłem nowe mieszkanie w dobrej cenie 🤖";
+    let text = onePerson ? "O godzinie " + getCurrentDateString() + " znalazłem pokój za " + price + "zł, oto link: </br>" + href : "O godzinie " + getCurrentDateString() + " znalazłem mieszkanie za (łącznie) " + (+price + +additionalPayments) + "zł, oto link: </br>" + href;
+    console.log(receiver, subject, text);
     transporter.sendMail({
-        from: '"Crawler" <' + process.env.GMAIL_EMAIL + '>', // sender address
-        to: "magicznasowa16@gmail.com,aleksandra.kucharczyk13@gmail.com", // list of receivers
-        subject: "🤖 Znalazłem nowe mieszkanie w dobrej cenie 🤖", // Subject line
-        text: "O godzinie " + getCurrentDateString() + " znalazłem mieszkanie za " + price + "zł, oto link:" + href, // plain text body
-        html: "O godzinie " + getCurrentDateString() + " znalazłem mieszkanie za (łącznie) " + (+price + +additionalPayments) + "zł, oto link: </br>" + href, // html body
+        from: '"Crawler" <' + process.env.GMAIL_EMAIL + '>',
+        to: receiver,
+        subject: subject,
+        text: text,
+        html: text
     }).then(info => {
         console.log({
             info
         });
     }).catch(console.error);
 }
+
+
+(async function main() {
+    cron.schedule('*/25 * * * *', () => {
+        crawl();
+
+    });
+})();
+
+
